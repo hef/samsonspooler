b21c1c3657956fa81532df581a1b0e82008f4536
hef <hef@pbrfrat.com>
2012-10-22 22:58:15 -0500

    url and view cleanup

--- samsonspooler/urls.py
+++ samsonspooler/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import redirect_to
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'samsonspooler.views.home', name='home'),
     # url(r'^samsonspooler/', include('samsonspooler.foo.urls')),
+    url('^$', redirect_to, {'url':'/thequeue/', 'permanent':False}),
     url('^thequeue/', include('thequeue.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
