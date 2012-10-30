ad250105ffe4d7ddffdf3ba69e89994a746e6a25
hef <hef@pbrfrat.com>
2012-10-29 23:54:58 -0500

    adjusting settings

--- config/production/settings.py
+++ config/production/settings.py
@@ -6,8 +6,11 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3'
+        'NAME': '/srv/www/spooler.arbitrarion.com/db.sqlite3'
     }
 }
+
+STATIC_ROOT = '/srv/www/spooler.arbitrarion.com/static/'
+MEDIA_ROOT = '/srv/www/spooler.arbitrarion.com/media/'
 
 ROOT_URLCONF = 'config.production.urls'
