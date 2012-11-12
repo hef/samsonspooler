980facff25d49221bec90f8c8fa1e7c3eac35f55
hef <hef@pbrfrat.com>
2012-11-11 22:30:23 -0600

    makeing manage.py production aware

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
+from socket import gethostname
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.development.settings")
+    if "li362-124" == gethostname():
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.production.settings")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.development.settings")
 
     from django.core.management import execute_from_command_line
 
